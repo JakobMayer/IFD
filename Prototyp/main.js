517096804b35080b92c999c4b63a38506e206926
JakobMayer <jakob.nathan.mayer@hs-furtwangen.de>
2022-06-24 13:53:35 +0200

    Kollage, Ausklappdaten und Barrierefreiheitstext hinzugefügt

---
+++ Prototyp/main.js
@@ -0,0 +1,4 @@
+function myFunction(){
+    anzeige = document.getElementById("result");
+    anzeige.innerHTML = "Durch das aktivieren von Barrierefeiheit werden Schrift und Buttons größer, die Sprache einfacher  und es gibt eine Sprachausgabe, welche die Elemente der Seite vorliest. Aus zeitlichen Gründen wurde dies hier nicht umgesetzt";
+}
